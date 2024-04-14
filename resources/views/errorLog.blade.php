75f4c64fb35c55a46dc8983258a0d4b39535622b
zaeem2396 <zaeemansari87@gmail.com>
2024-04-14 23:08:31 +0530

    imp: modified errorLog ui

--- resources/views/errorLog.blade.php
+++ resources/views/errorLog.blade.php
@@ -862,7 +862,7 @@
                     <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div style="margin-left: 5rem;">
 
-                            <h2 class="mt-6 text-md text-gray-900 dark:text-white">"Http_request" : <i>"{{$err['http_request']}}"</i></h2>
+                            <h2 class="mt-6 text-md text-gray-900 dark:text-white">"Request Method" : <i>"{{$err['http_request']}}"</i></h2>
                             <h2 class="mt-6 text-md text-gray-900 dark:text-white">"URL" : <i>"{{$err['url']}}"</i></h2>
                             <h2 class="mt-6 text-md text-gray-900 dark:text-white">"Params" : <i>
                                     <pre>"{{$params_json}}"</pre>
