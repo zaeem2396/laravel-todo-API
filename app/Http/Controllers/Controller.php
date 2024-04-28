c1140f3e715ce961e5d1c7e8d0ba6cf3a79d3164
zaeem2396 <zaeemansari87@gmail.com>
2024-04-29 00:40:31 +0530

    feat: cloudinary dependencies done

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
 
@@ -18,5 +20,14 @@ class Controller extends BaseController
     {
         $data['errorlog'] = DB::table('error_logs')->orderBy('id', 'desc')->get();
         return view('errorLog', $data);
+    }
+
+    public static function file(Request $request)
+    {
+        $file = $request->file('file');
+        $uploadedFile = Cloudinary::upload($file->getRealPath(), [
+            'folder' => 'laravel-todo-app'
+        ]);
+        return $uploadedFile;
     }
 }
